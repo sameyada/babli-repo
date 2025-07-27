19c752c3da1cefaf9bcb783d78592ba272dd38fe
unknown <sameer.up78@gmail.com>
2025-07-27 17:33:47 +0530

    sameer

--- main.tf
+++ main.tf
@@ -28,3 +28,7 @@ azurerm_resource_group "example" {
   name     = "example-resources"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
